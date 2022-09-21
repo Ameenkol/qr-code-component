2d1f80a571790321002aca557348c50931961515
Ameenkol <66386039+Ameenkol@users.noreply.github.com>
2022-09-21 17:51:15 +0100

    Update Qrcomp.jsx

--- src/containers/Qrcomp/Qrcomp.jsx
+++ src/containers/Qrcomp/Qrcomp.jsx
@@ -5,11 +5,11 @@ import './Qrcomp.scss';
 const Qrcomp = () => {
   return (
     <>
-      <div className='qrcomp'>
-        <div className='qrcomp__info'>
+      <div role="container" className='qrcomp'>
+        <div role="code" className='qrcomp__info'>
           <img className='qrcomp__info-img' src={images.qrcode} alt="qr-code" />
           
-          <div className='qrcomp__info-body'>
+          <div role="codeinfo" className='qrcomp__info-body'>
             <h5>Improve your front-end skills by building projects</h5>
             
             <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
